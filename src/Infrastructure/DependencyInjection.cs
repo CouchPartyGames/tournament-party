785aeb00ebbd92135cf314ab0ff84a00827d35af
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-06-27 17:57:07 -0400

    fix: add namespace for iconfiguration

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 namespace Infrastructure;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Persistence;

329ebc6f3f6c34de344ceaad8044ac882c944cb8
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-06-27 17:26:20 -0400

    feat: add postgresql

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,10 +1,16 @@
 namespace Infrastructure;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Persistence;
 
 public static class DependencyInjection {
 
-	public static IServiceCollection AddInfastructure(this IServiceCollection services) {
+	public static IServiceCollection AddInfastructure(this IServiceCollection services, IConfiguration config) {
+   		services.AddDbContext<TournamentContext>(options => 
+			options.UseNpgsql(config["ConnectionString"] )
+		);
+
 		return services;
 	}
 }
---
+++ src/Infrastructure/Persistence/TournamentContext.cs
@@ -0,0 +1,20 @@
+namespace Infrastructure.Persistence;
+
+//using System.Data.Entity.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+//using TournamentModel = CouchParty.TournamentBackend.Models.Tournament;
+//using MatchModel = CouchParty.TournamentBackend.Models.Match;
+//using TemplateModel = CouchParty.TournamentBackend.Models.Template;
+
+public sealed class TournamentContext : DbContext {
+
+	//public DbSet<TournamentModel> Tournament { get; set; }
+
+	//public DbSet<MatchModel> Match { get; set; }
+
+	//public DbSet<TemplateModel> Template { get; set; }
+
+	//public TournamentContext(DbContextOptions options) : base(options) {}
+
+}

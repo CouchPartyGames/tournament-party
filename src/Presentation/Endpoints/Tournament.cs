54d26b09867cf3542805702308b97b78b7759313
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2023-07-03 14:30:06 -0400

    feat: add mediatr

---
+++ src/Presentation/Endpoints/Tournament.cs
@@ -0,0 +1,32 @@
+﻿namespace Presentation.Endpoints;
+
+using Microsoft.AspNetCore.Routing;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public static class Tournament
+{
+
+    public static void AddTournamentEndpoints(this IEndpointRouteBuilder app)
+    {
+        //IMediator mediator;
+        var group = app.MapGroup("/v1/tournaments/");
+
+        group.MapGet("{id}", GetTournament)
+            .WithName("GetTournament");
+        //.AllowAnonymous();
+
+
+    }
+
+
+    /// <summary>
+    /// Returns a specific Tournament.
+    /// </summary>
+    public static async Results<Ok<Tournament>, NotFound> GetTournament(int id, IMediator mediator)
+    {
+        var result = await mediator.Send(new GetTournamentQuery());
+        return TypedResults.Ok(results);
+    }
+
+}

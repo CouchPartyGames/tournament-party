b40de6d68572fee3f1a2731a3b2245ee674aef80
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2023-07-05 14:29:41 -0400

    feat: add repositories interfaces

--- src/Application/Tournaments/GetTournamentHandler.cs
+++ src/Application/Tournaments/GetTournamentHandler.cs
@@ -1,11 +1,13 @@
 ﻿using MediatR;
 using Domain.Entities;
-
+using Application.Interfaces.Repositories;
 
 namespace Application.Tournaments;
 
 public sealed class GetTournamentHandler : IRequestHandler<GetTournamentQuery, Tournament>
 {
+    public GetTournamentHandler(ITournamentRepository repo) { }
+
     public async Task<Tournament> Handle(GetTournamentQuery request, CancellationToken cancellationToken)
     {
         var response = new Tournament();

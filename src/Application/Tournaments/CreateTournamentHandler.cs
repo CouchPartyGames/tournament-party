ae1d8a0c4670709299bb7f7619afdf56d1de1167
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-08-25 10:03:07 -0400

    chore: more ddd

--- src/Application/Tournaments/CreateTournamentHandler.cs
+++ src/Application/Tournaments/CreateTournamentHandler.cs
@@ -1,15 +1,23 @@
 ﻿using MediatR;
 using Domain.Tournament;
+using Domain.Abstractions;
+using Application.Interfaces.Repositories;
 
 namespace Application.Tournaments;
 
-public sealed class CreateTournamentHandler : IRequestHandler<CreateTournamentCommand, Tournament>
-{
+internal sealed class CreateTournamentHandler : IRequestHandler<CreateTournamentCommand, Tournament> {
 
-    public async Task<Tournament> Handle(CreateTournamentCommand request, CancellationToken cancellationToken)
-    {
-        var response = new Tournament();
-        return response;
+    private ITournamentRepository _tournamentRepository;
+
+    public CreateTournamentHandler(ITournamentRepository tournamentRepository) {
+        _tournamentRepository = tournamentRepository;
+    }
+
+    public async Task<Result<Tournament>> Handle(CreateTournamentCommand request, CancellationToken cancellationToken) {
+
+        var tournament = new Tournament();
+        //var tournament = await _tournamentRepository(tournament);
+        return Tournament.Id;
     }
 
 }
